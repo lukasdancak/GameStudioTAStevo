a68ebf34dd35b47e4746c8eea4bd0ff6a1f3c629
lukasdancak <lukas.dancak@gmail.com>
2022-08-30 22:56:35 +0200

    -parametricky fragment pre Scores - univerzalny pre vsetky controllery vsetkych hier -th:block v layoutGamestudio.html>brani nacitaniu komentarov a scores na homepage

--- src/main/java/sk/tuke/gamestudio/server/controller/GamestudioController.java
+++ src/main/java/sk/tuke/gamestudio/server/controller/GamestudioController.java
@@ -1,6 +1,7 @@
 package sk.tuke.gamestudio.server.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,13 +9,15 @@ public class GamestudioController {
 
 
     @RequestMapping("/")
-    public String mainPage(){
+    public String mainPage(Model model){
+        // skryje fragmenty pre komntare a score - nechcem ich na homepage
+        model.addAttribute("hideCommentsScores", true);
         return "gamestudio";
     }
 
     @RequestMapping("/games")
     public String gamesPage(){
-        return "gamestudio";
+        return "redirect:/gamestudio";
     }
 
 
